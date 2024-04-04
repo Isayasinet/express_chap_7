0c6ec9154c671fc48c903c211e486bbace8fd65b
Isayasinet <159775396+Isayasinet@users.noreply.github.com>
2024-04-04 19:02:45 +0200

    new chages

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3500;
+// built-in middleware to handle urlncoded data
+//`content-type: application/x-www-form-urlencoded
 
-app.get('^/$|/index(.html)?', (req, res) => {
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json());
+
+
+//serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('^/$|/index(.html)?', (req, res) => { //'^/$|/index(.html)?', '^' means start of the string, '$' means end of the string
     //res.sendFile('./views/index.html', { root: __dirname });
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
